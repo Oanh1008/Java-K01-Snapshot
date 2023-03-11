331766d3fd7b1edb85e1f12c0b6eec759521c2b4
Oanh1008 <poanh1002@gmail.com>
2023-03-11 13:46:04 +0700

    ex05 getUniqueNumbers(int[] arr)

---
+++ Lesson-01-DataStucture/lesson01-data-structure/src/exercices/Ex06.java
@@ -0,0 +1,9 @@
+package exercices;
+
+public class Ex06 {
+	public static void main(String[] args) {
+		int a = 10;
+		System.out.println(a++); // xuất a = 10 , a = 11
+		System.out.println(++a); // a + 1 = 12, xuất a = 12
+	}
+}
